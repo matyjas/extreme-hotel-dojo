a6b49bac3d5f4347e77fc2baede9dee4b10dc28d
Krishnakumar Govindarajan <kgovindarajan@LONC02G53BVDRJL.sea.corp.expecn.com>
2013-08-08 18:59:46 +0100

    Adding Bond Movie Solver

--- src/main/java/ExtremeStartup.java
+++ src/main/java/ExtremeStartup.java
@@ -15,7 +15,7 @@ import static com.google.common.collect.Lists.newArrayList;
 
 public class ExtremeStartup extends HttpServlet {
 
-    private static final List<Solver> solverChain = Lists.<Solver>newArrayList(new AddSolver(), new LargestNumberSolver(), new MultiplySolver());
+    private static final List<Solver> solverChain = Lists.<Solver>newArrayList(new AddSolver(), new LargestNumberSolver(), new MultiplySolver(), new BondMovieSolver());
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
